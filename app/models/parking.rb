340739593342d8b081e38c5da04dd2ef4cf3760a
nathanye <khalil54781@gmail.com>
2017-07-30 22:04:55 +0800

    rails_helper

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -22,6 +22,9 @@ class Parking < ApplicationRecord
   end
 
   def calculate_amount
+    puts "----"
+    puts self.parking_type
+    puts "----"
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
       if self.user.blank?
         self.amount = calculate_guest_term_amount # 一般费率
