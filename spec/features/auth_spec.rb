62c82e4bdd1d3bae31147cd6320bfb9b059a1309
nathanye <khalil54781@gmail.com>
2017-07-31 21:48:53 +0800

    login/logout

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -26,4 +26,23 @@ feature "register and login", :type => :feature do
     expect(user.email).to eq("foobar@example.com")
   end
 
+  scenario "login and logout" do
+    user = User.create!( :email => "foobar@example.com", :password => "12345678")
+
+    visit "/users/sign_in"
+
+    within("#new_user") do
+      fill_in "Email", with: "foobar@example.com"
+      fill_in "Password", with: "12345678"
+    end
+
+    click_button "Log in"  # 点击登入按钮
+
+    expect(page).to have_content("Signed in successfully")
+
+    click_link "登出"  # 点击主选单的登出超连结
+
+    expect(page).to have_content("Signed out successfully")
+  end
+
 end
